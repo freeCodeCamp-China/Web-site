21764cf3c3a2934e8a6098408b8b711e8d2310ef
dunyu.song <809175938@qq.com>
2023-07-22 19:25:04 +0800

    fix: change to css module

--- components/MainNavigator.tsx
+++ components/MainNavigator.tsx
@@ -1,30 +1,40 @@
-import React, { useState } from 'react';
+import React, { FC, useState } from 'react';
 
-export const MainNavigator: React.FC = () => {
+import styles from './MainNavigator.module.less';
+
+export const MainNavigator: FC = () => {
   const [isNavVisible, setIsNavVisible] = useState(true);
 
-  const handleToggleClick = () => {
-    setIsNavVisible(!isNavVisible);
-  };
+  const handleToggleClick = () => setIsNavVisible(!isNavVisible);
   return (
-    <nav className="clearfix">
-      <ul id="nav" className="clearfix">
-        <li className="head_logo">
-          <a href="https://www.freecodecamp.one">
-            <img src="./freecodecamp_logo.svg" className="init" alt="logo" />
+    <nav className={styles.clearfix}>
+      <ul className={styles.clearfix}>
+        <li className={styles.head_logo}>
+          <a href=".">
+            <img alt="logo" src="./freecodecamp_logo.svg" />
           </a>
         </li>
-        <li className="nav_bar" style={{ display: isNavVisible ? 'block' : 'none'}}>
+        <li className={styles.nav_bar} hidden={!isNavVisible}>
           <ul>
-            <li><a href="https://learn.freecodecamp.one">课程</a></li>
-            <li><a href="https://forum.freecodecamp.one">论坛</a></li>
-            <li><a href="https://www.freecodecamp.one/community">社区</a></li>
-            <li><a href="https://www.freecodecamp.one/volunteer">志愿者</a></li>
-            <li><a href="https://conf.freecodecamp.one">会议</a></li>
+            <li>
+              <a href="https://www.freecodecamp.org/learn">课程</a>
+            </li>
+            <li>
+              <a href="https://forum.freecodecamp.org">论坛</a>
+            </li>
+            <li>
+              <a href=".">社区</a>
+            </li>
+            <li>
+              <a href=".">志愿者</a>
+            </li>
+            <li>
+              <a href=".">会议</a>
+            </li>
           </ul>
         </li>
-        <li className="nav_toggle" onClick={handleToggleClick}>
-          <img src="./toggle_holder.png" className="init" alt="toggle" />
+        <li className={styles.nav_toggle} onClick={handleToggleClick}>
+          <img src="./toggle_holder.png" alt="toggle" />
         </li>
       </ul>
     </nav>
