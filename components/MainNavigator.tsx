5fe1d90bc2c419d68e838c714cc5cbd1f24ebb00
DunyuSong <809175938@qq.com>
2023-07-24 16:37:20 +0800

    feat: refactoring the navigation bar (#5)

--- components/MainNavigator.tsx
+++ components/MainNavigator.tsx
@@ -1,60 +1,43 @@
-import { Option, Select } from 'idea-react';
-import { observer } from 'mobx-react';
-import Link from 'next/link';
-import { FC } from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { FC, useState } from 'react';
 
-import { i18n, LanguageName } from '../models/Translation';
+import styles from './MainNavigator.module.less';
 
-const Name = process.env.NEXT_PUBLIC_SITE_NAME || '';
+export const MainNavigator: FC = () => {
+  const [isNavVisible, setIsNavVisible] = useState(true);
 
-export const MainNavigator: FC = observer(() => {
-  const { currentLanguage, t } = i18n;
+  const handleToggleClick = () => setIsNavVisible(!isNavVisible);
 
   return (
-    <Navbar
-      bg="primary"
-      variant="dark"
-      fixed="top"
-      expand="sm"
-      collapseOnSelect
-    >
-      <Container>
-        <Navbar.Brand href="/">{Name}</Navbar.Brand>
-
-        <Navbar.Toggle aria-controls="navbar-inner" />
-
-        <Navbar.Collapse id="navbar-inner">
-          <Nav className="me-auto">
-            <Link href="/component" passHref>
-              <Nav.Link>{t('component')}</Nav.Link>
-            </Link>
-            <Link href="/pagination" passHref>
-              <Nav.Link>{t('pagination')}</Nav.Link>
-            </Link>
-            <Link href="/scroll-list" passHref>
-              <Nav.Link>{t('scroll_list')}</Nav.Link>
-            </Link>
-            <Nav.Link
-              target="_blank"
-              href="https://github.com/idea2app/Next-Bootstrap-TS"
-            >
-              {t('source_code')}
-            </Nav.Link>
-          </Nav>
-
-          <Select
-            value={currentLanguage}
-            onChange={key => i18n.changeLanguage(key as typeof currentLanguage)}
-          >
-            {Object.entries(LanguageName).map(([key, name]) => (
-              <Option key={key} value={key}>
-                {name}
-              </Option>
-            ))}
-          </Select>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <nav className={`${styles.navigator} clearfix`}>
+      <ul className="clearfix">
+        <li className={styles.head_logo}>
+          <a href=".">
+            <img alt="logo" src="./freecodecamp_logo.svg" />
+          </a>
+        </li>
+        <li className={styles.nav_bar} hidden={!isNavVisible}>
+          <ul>
+            <li>
+              <a href="https://www.freecodecamp.org/chinese/learn/">课程</a>
+            </li>
+            <li>
+              <a href="https://forum.freecodecamp.org/c/chinese/">论坛</a>
+            </li>
+            <li>
+              <a href=".">社区</a>
+            </li>
+            <li>
+              <a href=".">志愿者</a>
+            </li>
+            <li>
+              <a href=".">会议</a>
+            </li>
+          </ul>
+        </li>
+        <li className={styles.nav_toggle} onClick={handleToggleClick}>
+          <img src="./toggle_holder.png" alt="toggle" />
+        </li>
+      </ul>
+    </nav>
   );
-});
+};
