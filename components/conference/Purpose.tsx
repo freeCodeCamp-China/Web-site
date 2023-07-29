2fc654e676c684a61e716f6f35a6b8a2cf3c41b3
huoyueyuyuan <huoyueyuyuan@126.com>
2023-07-29 09:34:44 +0800

    modified Purpose css config

--- components/conference/Purpose.tsx
+++ components/conference/Purpose.tsx
@@ -5,7 +5,7 @@ import styles from '../../styles/conference/Purpose.module.less';
 const Purpose: FC = () => {
 
     return (
-        <div className={`   ${styles.abstract}`} id="section_2">
+        <div className={`${styles.abstract}   text-center`} id="section_2">
             <div className={`${styles.container}   ${styles.animated}`}>
                 <h2 >大会宗旨</h2>
                 <p>freeCodeConf 是 freeCodeCamp 中国举办的每年一度大型联动技术大会，旨在联合 freeCodeCamp 各城市本地开发者社区，促进高度垂直、高度人才集中、高度明星企业集结的互联网与软件行业技术交流。第一届 freeCodeConf 将于 2018 年 11 月 10 日在全国 12 个城市同期举办，诚邀广大开发者共享技术盛宴。</p>
