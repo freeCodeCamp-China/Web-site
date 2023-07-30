7828b7f1b006867c87f66cd3fd3b152549efc00c
Shaoya HUANG <shaoyahuang22@gmail.com>
2023-07-30 14:06:15 +0800

    [feat] drawerNav on Conference Page; Add resolutions in package.json (#17)

--- pages/conference.tsx
+++ pages/conference.tsx
@@ -1,6 +1,7 @@
 import { observer } from 'mobx-react';
 import { Container } from 'react-bootstrap';
 
+import { DrawerNav } from '../components/DrawerNav';
 import { PageHead } from '../components/PageHead';
 import { i18n } from '../models/Translation';
 import styles from '../styles/Home.module.less';
@@ -13,9 +14,9 @@ const { t } = i18n;
 const ConferencePage = observer(() => (
   <Container as="main" className={styles.main}>
     <PageHead title="Hello Conference Page!" />
-
     <h1>Hello Conference Page!</h1>
     <h2>Let&apos;s get our hands dirty!</h2>
+    <DrawerNav />
   </Container>
 ));
 
