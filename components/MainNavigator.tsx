96c91bf7726b6895df71f1bb6791a184b2db9802
dunyu.song <809175938@qq.com>
2023-07-31 22:18:29 +0800

    feat: navigation with components

--- components/MainNavigator.tsx
+++ components/MainNavigator.tsx
@@ -1,52 +1,42 @@
 import { FC, useState } from 'react';
-import Nav from 'react-bootstrap/Nav';
+import { Nav,Navbar } from 'react-bootstrap';
 
 import styles from './MainNavigator.module.less';
 
 export const MainNavigator: FC = () => {
   const [isNavVisible, setIsNavVisible] = useState(true);
 
-  const handleToggleClick = () => setIsNavVisible(!isNavVisible);
-
   return (
-    <Nav className={`${styles.navigator} clearfix`}>
-      <Nav as="ul" className="clearfix">
-        <Nav.Item as="li" className={styles.head_logo}>
-          <Nav.Link href=".">
-            <img alt="logo" src="./freecodecamp_logo.svg" />
+    <Navbar expand="lg" bg="success" data-bs-theme="dark" className="shadow-sm">
+      <Navbar.Brand href="#">
+        <img alt="logo" src="./freecodecamp_logo.svg" />
+      </Navbar.Brand>
+      <Navbar.Toggle className={styles.nav_toggle} />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto align-items-center">
+          <Nav.Link
+            className="link-light"
+            href="https://www.freecodecamp.org/chinese/learn/"
+          >
+            课程
           </Nav.Link>
-        </Nav.Item>
-        <Nav.Item as="li" className={styles.nav_bar} hidden={!isNavVisible}>
-          <Nav as="ul">
-            <Nav.Item as="li">
-              <Nav.Link href="https://www.freecodecamp.org/chinese/learn/">
-                课程
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <Nav.Link href="https://forum.freecodecamp.org/c/chinese/">
-                论坛
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <Nav.Link href=".">社区</Nav.Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <Nav.Link href=".">志愿者</Nav.Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <Nav.Link href="/conference">会议</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Nav.Item>
-        <Nav.Item
-          as="li"
-          className={styles.nav_toggle}
-          onClick={handleToggleClick}
-        >
-          <img src="./toggle_holder.png" alt="toggle" />
-        </Nav.Item>
-      </Nav>
-    </Nav>
+          <Nav.Link
+            className="link-light"
+            href="https://forum.freecodecamp.org/c/chinese/"
+          >
+            论坛
+          </Nav.Link>
+          <Nav.Link className="link-light" href="#">
+            社区
+          </Nav.Link>
+          <Nav.Link className="link-light" href="#">
+            志愿者
+          </Nav.Link>
+          <Nav.Link className="link-light" href="/conference">
+            会议
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
