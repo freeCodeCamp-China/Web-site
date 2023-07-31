32fdb9ec9179fdaae3acd195c34bf6c1c790db72
dunyu.song <809175938@qq.com>
2023-07-31 23:26:36 +0800

    feat: delete less module optimize styles

--- components/MainNavigator.tsx
+++ components/MainNavigator.tsx
@@ -1,40 +1,24 @@
 import { FC, useState } from 'react';
-import { Nav,Navbar } from 'react-bootstrap';
-
-import styles from './MainNavigator.module.less';
+import { Nav, Navbar } from 'react-bootstrap';
 
 export const MainNavigator: FC = () => {
-  const [isNavVisible, setIsNavVisible] = useState(true);
-
   return (
     <Navbar expand="lg" bg="success" data-bs-theme="dark" className="shadow-sm">
       <Navbar.Brand href="#">
         <img alt="logo" src="./freecodecamp_logo.svg" />
       </Navbar.Brand>
-      <Navbar.Toggle className={styles.nav_toggle} />
+      <Navbar.Toggle className="border-0 shadow-none" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto align-items-center">
-          <Nav.Link
-            className="link-light"
-            href="https://www.freecodecamp.org/chinese/learn/"
-          >
+          <Nav.Link href="https://www.freecodecamp.org/chinese/learn/">
             课程
           </Nav.Link>
-          <Nav.Link
-            className="link-light"
-            href="https://forum.freecodecamp.org/c/chinese/"
-          >
+          <Nav.Link href="https://forum.freecodecamp.org/c/chinese/">
             论坛
           </Nav.Link>
-          <Nav.Link className="link-light" href="#">
-            社区
-          </Nav.Link>
-          <Nav.Link className="link-light" href="#">
-            志愿者
-          </Nav.Link>
-          <Nav.Link className="link-light" href="/conference">
-            会议
-          </Nav.Link>
+          <Nav.Link href="#">社区</Nav.Link>
+          <Nav.Link href="#">志愿者</Nav.Link>
+          <Nav.Link href="/conference">会议</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
