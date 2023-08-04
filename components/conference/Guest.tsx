2795ec21941b4de315017f87723b08930ff21af7
huoyueyuyuan <huoyueyuyuan@126.com>
2023-08-04 22:32:27 +0800

    finish guest component

---
+++ components/conference/Guest.tsx
@@ -0,0 +1,46 @@
+import * as fs from 'fs';
+import path from 'path';
+
+import styles from './Guest.module.less';
+
+type Guest = {
+  name: string;
+  pic: string;
+  position: string;
+};
+
+export interface GuestProps {
+  guests: Guest[];
+}
+
+export async function getConferenceData(): Promise<GuestProps> {
+  const filePath = path.join(process.cwd(), './components/data/GuestData.json');
+  const text = fs.readFileSync(filePath, 'utf8');
+  let guestJSON = JSON.parse(text) as GuestProps;
+  return guestJSON;
+}
+
+function GuestInfo({ guests }: GuestProps) {
+  return (
+    <div className="d-block text-center" id="guests">
+      <section
+        className={`${styles.container} mx-0 my-auto position-relative ${styles.animated}`}
+      >
+        <h2 className="fs-4 m-0 py-5">演讲嘉宾（持续更新）</h2>
+        <ul className="d-flex justify-content-between flex-wrap list-unstyled">
+          {guests.map((guest, index) => (
+            <li className={`${styles.media} text-center mt-1`} key={index}>
+              <img src={guest.pic} />
+              <h4 className="fs-6">{guest.name}</h4>
+              <p className="mt-3">
+                <span>{guest.position}</span>
+              </p>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+}
+
+export default GuestInfo;
--- pages/conference.tsx
+++ pages/conference.tsx
@@ -1,23 +1,41 @@
 import { observer } from 'mobx-react';
+import { InferGetServerSidePropsType } from 'next';
+import { PureComponent, ReactNode } from 'react';
 import { Container } from 'react-bootstrap';
 
 import { ConferenceBase } from '../components/conference/ConferenceBase';
+import GuestInfo, {
+  getConferenceData,
+  GuestProps,
+} from '../components/conference/Guest';
 import { DrawerNav } from '../components/DrawerNav';
 import { PageHead } from '../components/PageHead';
 import { i18n } from '../models/Translation';
 import styles from '../styles/Home.module.less';
 import { withTranslation } from './api/core';
 
-export const getServerSideProps = withTranslation();
+export const getServerSideProps = withTranslation(async function () {
+  const guestsData = await getConferenceData();
+  const { guests } = guestsData;
+  return {
+    props: { guests },
+  };
+});
 
 const { t } = i18n;
 
-const ConferencePage = observer(() => (
-  <Container as="main" className={styles.main}>
-    <PageHead title="Hello Conference Page!" />
-    <DrawerNav />
-    <ConferenceBase />
-  </Container>
-));
-
-export default ConferencePage;
+@observer
+export default class ConferencePage extends PureComponent<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> {
+  render() {
+    return (
+      <Container as="main" className={styles.main}>
+        <PageHead title="Hello Conference Page!" />
+        <DrawerNav />
+        <ConferenceBase />
+        <GuestInfo guests={this.props.guests} />
+      </Container>
+    );
+  }
+}
