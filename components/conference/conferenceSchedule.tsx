135b1992673a2f421950b6b9437324f091a6e568
huoyueyuyuan <huoyueyuyuan@126.com>
2023-08-08 21:12:49 +0800

    finish base

--- components/conference/conferenceSchedule.tsx
+++ components/conference/conferenceSchedule.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { Nav, Tab, Tabs } from 'react-bootstrap';
 
 import citySchedulesData from './../data/CitySchedulesData.json';
 import styles from './ConferenceSchedule.module.less';
@@ -21,55 +22,63 @@ export interface CityScheduleProps {
 
 const { citySchedulesList } = citySchedulesData as CityScheduleProps;
 
-export const CityScheduleInfo: FC = () => (
-  <div className="text-center" id="schedule">
-    <section
-      className={`${styles.container} mx-auto my-0 position-relative ${styles.animated}`}
-    >
-      <h2>大会日程与报名通道</h2>
-      <div className={styles.timeline}>
-        {citySchedulesList?.map(({ city }) => (
-          <span className="open p-3" key={city}>
-            {city}
-          </span>
-        ))}
-      </div>
-      <div>
-        {citySchedulesList?.map(({ city, href, schedules }) => (
-          <div
-            className="table_box"
-            data-city={city}
-            style={{ display: 'block' }}
-            key={city}
-          >
-            <a target="_blank" href={href} rel="noreferrer">
-              {href ? '点此报名' : '报名通道即将开启'}
-            </a>
-            {schedules === undefined || schedules?.length === 0 ? (
-              <></>
-            ) : (
-              <table>
-                <thead>
-                  <tr>
-                    <th>时间</th>
-                    <th>主题</th>
-                    <th>演讲嘉宾</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {schedules.map(({ time, topic, guest }) => (
-                    <tr key={topic}>
-                      <td>{time}</td>
-                      <td>{topic}</td>
-                      {guest !== undefined ? <td>{guest}</td> : <></>}
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            )}
-          </div>
-        ))}
-      </div>
-    </section>
-  </div>
-);
+export const CityScheduleInfo: FC = () => {
+  return (
+    <div className="text-center" id="schedule">
+      <section
+        className={`${styles.container} mx-auto my-0 position-relative ${styles.animated}`}
+      >
+        <h2>大会日程与报名通道</h2>
+
+        <Tabs
+          variant="pills"
+          defaultActiveKey={citySchedulesList[0].city}
+          className="mb-3"
+          justify
+        >
+          {citySchedulesList?.map(({ city, href, schedules }) => (
+            <Tab className="open p-3" key={city} eventKey={city} title={city}>
+              <div
+                className="table_box"
+                data-city={city}
+                style={{ display: 'block' }}
+                key={city}
+              >
+                <a
+                  target="_blank"
+                  href={href}
+                  rel="noreferrer"
+                  className="text-start"
+                >
+                  {href ? '点此报名' : '报名通道即将开启'}
+                </a>
+                {schedules === undefined || schedules?.length === 0 ? (
+                  <></>
+                ) : (
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>时间</th>
+                        <th>主题</th>
+                        <th>演讲嘉宾</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {schedules.map(({ time, topic, guest }) => (
+                        <tr key={topic}>
+                          <td>{time}</td>
+                          <td>{topic}</td>
+                          {guest !== undefined ? <td>{guest}</td> : <></>}
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                )}
+              </div>
+            </Tab>
+          ))}
+        </Tabs>
+      </section>
+    </div>
+  );
+};
