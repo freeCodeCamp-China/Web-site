6b96b021f4d04865dfc33d2fddf3fdb805a32bb2
huoyueyuyuan <huoyueyuyuan@126.com>
2023-08-09 00:24:03 +0800

    modified css config

--- components/conference/conferenceSchedule.tsx
+++ components/conference/conferenceSchedule.tsx
@@ -1,14 +1,10 @@
 import { FC } from 'react';
-import { Nav, Tab, Tabs } from 'react-bootstrap';
+import { Tab, Tabs } from 'react-bootstrap';
 
 import citySchedulesData from './../data/CitySchedulesData.json';
 import styles from './ConferenceSchedule.module.less';
 
-type Schedule = {
-  time: string;
-  topic: string;
-  guest?: string;
-};
+type Schedule = Record<'time' | 'topic', string> & { guest?: string };
 
 type CitySchedules = {
   city: string;
@@ -28,47 +24,46 @@ export const CityScheduleInfo: FC = () => {
       <section
         className={`${styles.container} mx-auto my-0 position-relative ${styles.animated}`}
       >
-        <h2>大会日程与报名通道</h2>
+        <h2 className="fs-4 py-5">大会日程与报名通道</h2>
 
         <Tabs
           variant="pills"
           defaultActiveKey={citySchedulesList[0].city}
-          className="mb-3"
+          className={`${styles.cityLine} w-75 mb-2`}
           justify
         >
-          {citySchedulesList?.map(({ city, href, schedules }) => (
-            <Tab className="open p-3" key={city} eventKey={city} title={city}>
-              <div
-                className="table_box"
-                data-city={city}
-                style={{ display: 'block' }}
-                key={city}
-              >
+          {citySchedulesList.map(({ city, href, schedules }) => (
+            <Tab className="p-3" key={city} eventKey={city} title={city}>
+              <div className={styles.table_box} data-city={city} key={city}>
                 <a
                   target="_blank"
                   href={href}
                   rel="noreferrer"
-                  className="text-start"
+                  className="px-3 pb-3 text-start d-block"
                 >
                   {href ? '点此报名' : '报名通道即将开启'}
                 </a>
                 {schedules === undefined || schedules?.length === 0 ? (
                   <></>
                 ) : (
-                  <table>
+                  <table className="text-start">
                     <thead>
                       <tr>
-                        <th>时间</th>
-                        <th>主题</th>
-                        <th>演讲嘉宾</th>
+                        <th className="px-5 py-3">时间</th>
+                        <th className="px-3 py-3">主题</th>
+                        <th className="px-3 py-3">演讲嘉宾</th>
                       </tr>
                     </thead>
                     <tbody>
                       {schedules.map(({ time, topic, guest }) => (
                         <tr key={topic}>
-                          <td>{time}</td>
-                          <td>{topic}</td>
-                          {guest !== undefined ? <td>{guest}</td> : <></>}
+                          <td className="px-3 py-2">{time}</td>
+                          <td className="py-2">{topic}</td>
+                          {guest !== undefined ? (
+                            <td className="py-2">{guest}</td>
+                          ) : (
+                            <></>
+                          )}
                         </tr>
                       ))}
                     </tbody>
