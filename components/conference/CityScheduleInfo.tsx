8c60627dd8561bb9d5e015762f74dbf43ea45c15
taitouxiangshang <huoyueyuyuan@126.com>
2023-08-28 07:41:46 +0800

    Unified city label style

--- components/conference/CityScheduleInfo.tsx
+++ components/conference/CityScheduleInfo.tsx
@@ -1,27 +1,45 @@
-import { FC } from 'react';
-import { Container, Tab, Tabs } from 'react-bootstrap';
+import { FC, useState } from 'react';
+import { Tab, Tabs } from 'react-bootstrap';
 
 import citySchedulesData from '../../data/conference/CitySchedulesData.json';
 import styles from './CityScheduleInfo.module.less';
 
-export const CityScheduleInfo: FC = () => (
-  <Container>
-    <section className="container mx-auto my-0 position-relative animated text-center">
-      <h2 id="schedule" className="fs-4 py-5">
-        大会日程与报名通道
-      </h2>
+export const CityScheduleInfo: FC = () => {
+  const [currentCity, setCurrentCity] = useState(citySchedulesData[0]?.city); // 初始化为第一个城市
 
-      <Tabs className="w-100 mb-2" variant="pills" justify>
-        {citySchedulesData.map(({ city, href, schedules }) => (
-          <Tab className="p-3" key={city} eventKey={city} title={city}>
-            <div
-              className={`${styles.table_box} w-100 overflow-x-auto`}
-              data-city={city}
+  return (
+    <>
+      <section className="container mx-auto my-0 position-relative text-center">
+        <h2 id="schedule" className="fs-4 py-5">
+          大会日程与报名通道
+        </h2>
+
+        <Tabs
+          className="flex-nowrap overflow-x-auto"
+          variant="pills"
+          fill
+          activeKey={currentCity}
+          onSelect={key => setCurrentCity(key)}
+        >
+          {citySchedulesData.map(({ city }, index) => (
+            <Tab
+              className=""
               key={city}
+              eventKey={city}
+              title={city}
+              id={index}
             >
+              <div data-city={city} key={city}></div>
+            </Tab>
+          ))}
+        </Tabs>
+        {citySchedulesData
+          .filter(citySchedules => citySchedules.city === currentCity)
+          .map(({ city, href, schedules }) => (
+            <div className={styles.table_box} key={city}>
               <div className="d-flex">
                 <a
-                  className="px-3 pb-3 text-start"
+                  className="p-3 text-start"
                   target="_blank"
                   href={href}
                   rel="noreferrer"
@@ -50,9 +68,8 @@ export const CityScheduleInfo: FC = () => (
                 </table>
               )}
             </div>
-          </Tab>
-        ))}
-      </Tabs>
-    </section>
-  </Container>
-);
+          ))}
+      </section>
+    </>
+  );
+};
