e6079b42dd9c9f8bfb4c55556589f04c3d09d6fc
taitouxiangshang <huoyueyuyuan@126.com>
2023-08-28 16:57:20 +0800

    rollback  CityScheduleInfo

--- components/conference/CityScheduleInfo.tsx
+++ components/conference/CityScheduleInfo.tsx
@@ -1,39 +1,27 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { Container, Tab, Tabs } from 'react-bootstrap';
 
 import citySchedulesData from '../../data/conference/CitySchedulesData.json';
 import styles from './CityScheduleInfo.module.less';
 
-export const CityScheduleInfo: FC = () => {
-  const [selectCity, setSelectCity] = useState(citySchedulesData[0].city); // 初始化为第一个城市
-  return (
-    <Container>
-      <section className="container mx-auto my-0 position-relative animated text-center">
-        <h2 id="schedule" className="fs-4 py-3">
-          大会日程与报名通道
-        </h2>
+export const CityScheduleInfo: FC = () => (
+  <Container>
+    <section className="container mx-auto my-0 position-relative animated text-center">
+      <h2 id="schedule" className="fs-4 py-5">
+        大会日程与报名通道
+      </h2>
 
-        <Tabs
-          className="flex-nowrap overflow-x-auto py-2"
-          variant="pills"
-          fill
-          activeKey={selectCity}
-          onSelect={key => setSelectCity(key)}
-        >
-          {citySchedulesData.map(({ city }) => (
-            <Tab className="" key={city} eventKey={city} title={city} id={city}>
-              <div data-city={city} key={city}></div>
-            </Tab>
-          ))}
-        </Tabs>
-
-        {citySchedulesData
-          .filter(({ city }) => city === selectCity)
-          .map(({ city, href, schedules }) => (
-            <div className={styles.table_box} key={city}>
+      <Tabs className="w-100 mb-2" variant="pills" justify>
+        {citySchedulesData.map(({ city, href, schedules }) => (
+          <Tab className="p-3" key={city} eventKey={city} title={city}>
+            <div
+              className={`${styles.table_box} w-100 overflow-x-auto`}
+              data-city={city}
+              key={city}
+            >
               <div className="d-flex">
                 <a
-                  className="px-3 py-2 text-start"
+                  className="px-3 pb-3 text-start"
                   target="_blank"
                   href={href}
                   rel="noreferrer"
@@ -41,30 +29,30 @@ export const CityScheduleInfo: FC = () => {
                   {href ? '点此报名' : '报名通道即将开启'}
                 </a>
               </div>
-
               {schedules?.[0] && (
-                <table className="text-start">
+                <table className="text-start w-100">
                   <thead>
                     <tr>
-                      <th className="p-2 align-top">时间</th>
-                      <th className="p-2 align-top">主题</th>
-                      <th className="p-2 align-top">演讲嘉宾</th>
+                      <th className="p-3">时间</th>
+                      <th className="p-3">主题</th>
+                      <th className="p-3">演讲嘉宾</th>
                     </tr>
                   </thead>
                   <tbody>
-                    {schedules.map(({ time, topic, guest }, index) => (
-                      <tr key={index}>
-                        <td className="p-2 align-top">{time}</td>
-                        <td className="p-2 align-top">{topic}</td>
-                        <td className="p-2 align-top">{guest}</td>
+                    {schedules.map(({ time, topic, guest }) => (
+                      <tr key={topic}>
+                        <td className="px-3 py-2">{time}</td>
+                        <td className="py-2">{topic}</td>
+                        <td className="py-2">{guest}</td>
                       </tr>
                     ))}
                   </tbody>
                 </table>
               )}
             </div>
-          ))}
-      </section>
-    </Container>
-  );
-};
+          </Tab>
+        ))}
+      </Tabs>
+    </section>
+  </Container>
+);
