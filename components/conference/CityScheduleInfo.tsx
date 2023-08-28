98d87bdf64697850519acdcdadda13d55d507747
taitouxiangshang <huoyueyuyuan@126.com>
2023-08-28 19:19:03 +0800

    try

--- components/conference/CityScheduleInfo.tsx
+++ components/conference/CityScheduleInfo.tsx
@@ -19,7 +19,7 @@ export const CityScheduleInfo: FC = () => {
           variant="pills"
           fill
           activeKey={selectedCity}
-          onSelect={k => k || setSelectedCity(k)}
+          onSelect={k => k && setSelectedCity(k)}
         >
           {citySchedulesData.map(({ city }) => (
             <Tab key={city} eventKey={city} title={city} id={city}>
