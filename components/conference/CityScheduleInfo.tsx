ea29ca9d1daa86a8dcad1586111b0a88df2c9f3a
taitouxiangshang <huoyueyuyuan@126.com>
2023-08-28 11:55:57 +0800

    handle build error

--- components/conference/CityScheduleInfo.tsx
+++ components/conference/CityScheduleInfo.tsx
@@ -26,44 +26,6 @@ export const CityScheduleInfo: FC = () => {
           </Tab>
         ))}
       </Tabs>
-
-      {citySchedulesData
-        .filter(citySchedules => citySchedules.city === selectedCity)
-        .map(({ city, href, schedules }) => (
-          <div className={styles.table_box} key={city}>
-            <div className="d-flex">
-              <a
-                className="p-3 text-start"
-                target="_blank"
-                href={href}
-                rel="noreferrer"
-              >
-                {href ? '点此报名' : '报名通道即将开启'}
-              </a>
-            </div>
-
-            {schedules?.[0] && (
-              <table className="text-start w-100">
-                <thead>
-                  <tr>
-                    <th className="p-3 align-top">时间</th>
-                    <th className="p-3 align-top">主题</th>
-                    <th className="p-3 align-top">演讲嘉宾</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {schedules.map(({ time, topic, guest }) => (
-                    <tr key={topic}>
-                      <td className="px-3 py-2 align-top">{time}</td>
-                      <td className="py-2 align-top">{topic}</td>
-                      <td className="py-2 align-top">{guest}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            )}
-          </div>
-        ))}
     </section>
   );
 };
