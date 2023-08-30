ec71338d12745499df9dfef677abb878828321ce
taitouxiangshang <huoyueyuyuan@126.com>
2023-08-30 11:48:25 +0800

    use Button

--- components/conference/CityScheduleInfo.tsx
+++ components/conference/CityScheduleInfo.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Container, Nav, Tab } from 'react-bootstrap';
+import { Button, Container, Nav, Tab } from 'react-bootstrap';
 
 import citySchedulesData from '../../data/conference/CitySchedulesData.json';
 import styles from './CityScheduleInfo.module.less';
@@ -25,37 +25,33 @@ export const CityScheduleInfo: FC = () => (
 
         <Tab.Content>
           {citySchedulesData.map(({ city, href, schedules }) => (
-            <Tab.Pane key={city} eventKey={city}>
-              <div className={styles.table_box} key={city}>
-                <a
-                  className="px-3 py-2 text-start d-flex w-50"
-                  target="_blank"
-                  href={href}
-                  rel="noreferrer"
-                >
+            <Tab.Pane key={city} eventKey={city} className={styles.table_box}>
+              <div className="d-flex justify-content-start py-3">
+                <Button as="a" variant="primary" target="_blank" href={href}>
                   {href ? '点此报名' : '报名通道即将开启'}
-                </a>
-                {schedules?.[0] && (
-                  <table className="text-start">
-                    <thead>
-                      <tr>
-                        <th className="p-2 align-middle">时间</th>
-                        <th className="p-2 align-middle">主题</th>
-                        <th className="p-2 align-middle">演讲嘉宾</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {schedules.map(({ time, topic, guest }) => (
-                        <tr key={time}>
-                          <td className="p-2 align-middle">{time}</td>
-                          <td className="p-2 align-middle">{topic}</td>
-                          <td className="p-2 align-middle">{guest}</td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                )}
+                </Button>
               </div>
+
+              {schedules?.[0] && (
+                <table className="text-start w-100">
+                  <thead>
+                    <tr>
+                      <th className="p-2 align-middle">时间</th>
+                      <th className="p-2 align-middle">主题</th>
+                      <th className="p-2 align-middle">演讲嘉宾</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {schedules.map(({ time, topic, guest }) => (
+                      <tr key={time}>
+                        <td className="p-2 align-middle">{time}</td>
+                        <td className="p-2 align-middle">{topic}</td>
+                        <td className="p-2 align-middle">{guest}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              )}
             </Tab.Pane>
           ))}
         </Tab.Content>
